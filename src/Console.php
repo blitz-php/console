99f144da74c6ea51f4aa0bbfde109417cb651514
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2026-02-26 14:54:36 +0100

    fix: improve error handling and logging in onError method

--- src/Console.php
+++ src/Console.php
@@ -472,10 +472,14 @@ class Console extends Application
      */
     protected function onError(Throwable $e, int $exitCode): void
     {
-        $this->io()->error($e->getMessage(), true);
+        if ($this->logger['instance'] !== null) {
+            $this->logger['instance']->error('[' . $this->name() . '] ' . $e->getMessage(), ['exception' => $e]);
+        }
 
         if ($this->flags['debug']) {
-            $this->io()->error($e->getTraceAsString(), true);
+            $this->outputHelper()->printTrace($e);
+        } else {
+            $this->io()->error($e->getMessage(), true);
         }
 
         exit($exitCode);
